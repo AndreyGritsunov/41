ec0ff78062a6ab19d19da2dc9a86ea7ac5943624
unknown <gritsunov85@gmail.com>
2022-08-30 09:27:02 +0600

    the end

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int i = 0;
+int totalSum = 0;
+while(i == 0){
+    Console.WriteLine("Введите число, или завершите ввод поставив 'Y': ");
+    var number = Console.ReadLine();
+    if(number == "Y"){
+        i++;
+    }
+    else{
+        if(Convert.ToInt32(number) > 0){
+            totalSum = totalSum + 1;
+        }
+    }
+}
+
+Console.Write("Вы ввели " + Convert.ToString(totalSum)+ " больше 0");
